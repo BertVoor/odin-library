864569fdd5e599ec7b68d80b94f6bbc959f37ebe
Bert <voorjansbert@gmail.com>
2024-09-05 23:30:22 +0200

    Add basic function to display book cards

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const myLibrary = [];
 
 const showBtn = document.getElementById("showDialog");
 const dialog = document.getElementById("newBookDialog");
+const confirmBtn = dialog.querySelector("#confirmBtn");
 
-const newAuthor = dialog.getElementById("author");
-const newTitle = dialog.getElementById("title");
-const newPages = dialog.getElementById("pages");
-const newReadStatus = dialog.getElementById("readStatus");
+const newAuthor = dialog.querySelector("#author");
+const newTitle = dialog.querySelector("#title");
+const newPages = dialog.querySelector("#pages");
+const newReadStatus = dialog.querySelector("#readStatus");
+
+const bookDisplay = document.getElementById("bookDisplay");
 
 showBtn.addEventListener("click", () => {
 	dialog.showModal();
@@ -35,4 +38,21 @@ function addBookToLibrary() {
 ////////////////////////
 const bookOne = new Book("titel", "schrijver", 420, true);
 const bookTwo = new Book("hello", "world", 666, false);
+myLibrary.push(bookOne);
+myLibrary.push(bookTwo);
 ////////////////////////
+
+function updateBookDisplay(book) {
+	const bookCardHTML = `<div class="bookCard">
+	<h2 class="bookTitle">${book.title}</h2>
+	<p class="bookAuthor">${book.author}</p>
+	<p class="bookPages">${book.pages} pages</p>
+	<p class="bookReadStatus">read</p>
+	</div>`;
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.insertAdjacentHTML("beforeend", bookCardHTML);
+	bookDisplay.appendChild(card);
+}
+
+myLibrary.forEach((book) => updateBookDisplay(book));
